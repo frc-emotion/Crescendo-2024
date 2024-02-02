7d786b56fb60e9da82dbe0ddd1bbd7c683ccc717
PreenaM <pmaruthavel7424@student.palomar.edu>
2024-02-01 17:18:56 -0800

    intake w spark pid

---
+++ src/main/java/frc/robot/commands/PivotIntake.java
@@ -0,0 +1,33 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
+
+public class PivotIntake extends Command {
+
+    private Intake intake;
+
+    public PivotIntake(Intake intake){
+        this.intake = intake;
+        addRequirements(intake);
+    }
+
+    @Override
+    public void initialize() {
+        intake.toggleEndState();
+    }
+
+    @Override
+    public void execute() {
+        intake.pivot();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+}
