5bb1541165f58ff25f486b55c82f74d4b8a5b2e3
“Arshan” <arshan@arshan.dev>
2024-02-08 17:23:18 -0800

    didnt do much

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,11 +9,15 @@ import frc.robot.Constants.OIConstants;
 import frc.robot.commands.SwerveXboxCommand;
 import frc.robot.subsystems.SwerveSubsystem;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.pathfinding.LocalADStar;
 import com.pathplanner.lib.pathfinding.Pathfinding;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 // import frc.robot.subsystems.VisionSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -37,10 +41,13 @@ public class RobotContainer {
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController = new CommandXboxController(OIConstants.kDriverControllerPort);
 
+  private final SendableChooser<Command> autoChooser;
+
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
+    autoChooser = AutoBuilder.buildAutoChooser();
 
     m_SwerveSubsystem.setDefaultCommand(
         new SwerveXboxCommand(
@@ -56,6 +63,8 @@ public class RobotContainer {
 
     // Configure the trigger bindings
     configureBindings();
+
+    SmartDashboard.putData("Auto Chooser", autoChooser);
   }
 
   /**
@@ -84,9 +93,10 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    return m_SwerveSubsystem.navigateToPose(
-      new Pose2d(2, 2, m_SwerveSubsystem.getRotation2d())
-    );
+    // return m_SwerveSubsystem.navigateToPose(
+    //   new Pose2d(2, 2, m_SwerveSubsystem.getRotation2d())
+    // );
+    return autoChooser.getSelected();
   }
 
 }
