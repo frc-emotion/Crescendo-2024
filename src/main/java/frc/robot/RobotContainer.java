3d038ebc2ea2db230ae127b110f1cf99d3a04848
Arshan <arshan@arshan.dev>
2024-02-10 23:05:30 -0800

    doing controller bindings comes later

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,9 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OIConstants;
-import frc.robot.commands.PivotIntake;
-import frc.robot.commands.RunIntake;
-import frc.robot.commands.SwerveXboxCommand;
-import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.SwerveSubsystem;
-import edu.wpi.first.wpilibj.XboxController;
-
+// import frc.robot.subsystems.VisionSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
@@ -34,39 +26,7 @@ import frc.robot.subsystems.SwerveSubsystem;
  */
 public class RobotContainer {
 
-  public static final IntakeSubsystem m_IntakeSubsystem = new IntakeSubsystem();
-
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OIConstants.kDriverControllerPort);
-
-  private final CommandXboxController m_operatorController =
-      new CommandXboxController(OIConstants.kOperatorControllerPort);
-
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-
-    m_SwerveSubsystem.setDefaultCommand(
-    new SwerveXboxCommand(
-      m_SwerveSubsystem,
-      () -> -m_driverController.getRawAxis(OIConstants.kDriverYAxis),
-      () -> -m_driverController.getRawAxis(OIConstants.kDriverXAxis),
-      () -> m_driverController.getRawAxis(OIConstants.kDriverRotAxis),
-      () -> !m_driverController.a().getAsBoolean(),
-      () -> m_driverController.leftBumper().getAsBoolean(),
-      () -> m_driverController.rightBumper().getAsBoolean(),
-      () -> m_driverController.getRightTriggerAxis(),
-      () -> m_driverController.getLeftTriggerAxis()
-    ));
-
-    // FIX
-    // m_IntakeSubsystem.setDefaultCommand(new RunIntake(m_IntakeSubsystem, () -> m_operatorController.rightBumper.getAsBoolean(), () -> m_operatorController.leftBumper.getAsBoolean()));
-        
-    // Configure the trigger bindings
-    configureBindings();
-  }
-
-  // The robot's subsystems and commands are defined here...
+    // The robot's subsystems and commands are defined here...
     public static final SwerveSubsystem m_SwerveSubsystem = new SwerveSubsystem();
     public static final ShooterSubsystem m_shooterSubsystem = new ShooterSubsystem();
 
@@ -107,14 +67,7 @@ public class RobotContainer {
         configureBindings();
     }
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    //m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-
-    new JoystickButton(m_operatorController.getHID(), XboxController.Button.kB.value).onTrue(new PivotIntake(m_IntakeSubsystem));
-  }
-
-/**
+    /**
      * Use this method to define your trigger->command mappings. Triggers can be
      * created via the
      * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
