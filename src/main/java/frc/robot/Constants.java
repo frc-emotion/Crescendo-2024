3aa626425a3268ccc4f434179aeeaaf592de19c4
Jason Ballinger <jasonballinger_dev@yahoo.com>
2024-02-27 14:29:02 -0800

    peyton :(

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -48,7 +48,6 @@ public final class Constants {
         public static final double INTAKE_DEADZONE = 0.2;
         public static final double kPersianSpeedMultiplier = 2.0;
         public static final double kSpeedDivideAdjustment = 2.0;
-        
     }
 
     public static final class ModuleConstants {
@@ -103,7 +102,6 @@ public final class Constants {
             new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
             new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
             new Translation2d(-kWheelBase / 2, -kTrackWidth / 2)
-            
         );
 
         /**CAN IDs for all drive motors */
@@ -128,7 +126,7 @@ public final class Constants {
         public static final boolean kBackRightDriveEncoderReversed = false;
 
         public static final int kFrontLeftDriveAbsoluteEncoderPort = 4;
-        public static final int kBackLeftDriveAbsoluteEncoderPort = 3;  //2143
+        public static final int kBackLeftDriveAbsoluteEncoderPort = 3; //2143
         public static final int kFrontRightDriveAbsoluteEncoderPort = 1;
         public static final int kBackRightDriveAbsoluteEncoderPort = 2;
 
@@ -144,12 +142,16 @@ public final class Constants {
         /**
          * Calculate by Positioning wheels at zero manually and reading absolute encoder values
          */
-        
-        public static final double kFrontLeftDriveAbsoluteEncoderOffsetRad =    19.42 / 360; 
-        public static final double kBackLeftDriveAbsoluteEncoderOffsetRad =     -35.94 / 360;
-        public static final double kFrontRightDriveAbsoluteEncoderOffsetRad =   -89.03 / 360;
-        public static final double kBackRightDriveAbsoluteEncoderOffsetRad =    64.16 / 360;
-        //  public static final double kFrontLeftDriveAbsoluteEncoderOffsetRad = -Units.degreesToRadians(180 + 0.5); 
+
+        public static final double kFrontLeftDriveAbsoluteEncoderOffsetRad =
+            19.42 / 360;
+        public static final double kBackLeftDriveAbsoluteEncoderOffsetRad =
+            -35.94 / 360;
+        public static final double kFrontRightDriveAbsoluteEncoderOffsetRad =
+            -89.03 / 360;
+        public static final double kBackRightDriveAbsoluteEncoderOffsetRad =
+            64.16 / 360;
+        //  public static final double kFrontLeftDriveAbsoluteEncoderOffsetRad = -Units.degreesToRadians(180 + 0.5);
         //  public static final double kBackLeftDriveAbsoluteEncoderOffsetRad = -Units.degreesToRadians(180 + 39.8 - 3.1);
         //  public static final double kFrontRightDriveAbsoluteEncoderOffsetRad = -Units.degreesToRadians(180 + -154.098 - 0.7);
         //  public static final double kBackRightDriveAbsoluteEncoderOffsetRad =  -Units.degreesToRadians(157.7 - 4.7);
@@ -237,7 +239,9 @@ public final class Constants {
             kMaxAngularAccelerationRadiansPerSecondSquared
         );
 
-		public static final double INTAKE_TIMEOUT = 0;
+        public static final double INTAKE_TIMEOUT = 0;
+
+        public static final double SHOOTER_SPEED_RPM = 2000.0;
     }
 
     public static final class CameraConstants {
@@ -295,20 +299,21 @@ public final class Constants {
         public static final double kMaxOutputError = 0;
 
         // Speeds
-        
+
         public static final double kFeedSpeed = 0.25;
         public static final double kShootSpeedRotationsPerSecond = 0;
         public static final double kMaxSpeedRotationsPerSecond = 0;
         public static final double kMaxSpeedRotationsPerSecondSquared = 0;
         public static final int BREAK_SENSOR_PORT = 0;
-		public static final double GEAR_REDUCTION = 0.5;
+        public static final double GEAR_REDUCTION = 0.5;
         public static final int CURRENT_LIMIT_SMART = 20;
 
-        public static final double[] PRESET_SPEEDS = {100, 4000};
+        public static final double[] PRESET_SPEEDS = { 100, 4000 };
         public static final double IDLE_SPEED = PRESET_SPEEDS[0];
     }
 
     public static final class PivotConstants {
+
         public static final int PIVOT_PORT = 20;
 
         public static final double PIVOT_KP = 10.0;
@@ -326,32 +331,29 @@ public final class Constants {
         public static final double PIVOT_MIN_REVOLUTION = 5;
         public static final double PIVOT_MAX_REVOLUTION = 83;
         public static final double PIVOT_THRESHOLD = 6;
-        public static final double[] PIVOT_POSITIONS = {-60.0, 0};
+        public static final double[] PIVOT_POSITIONS = { -60.0, 0 };
 
         public static final double TRIGGER_THRESHOLD = 0.3;
         public static final int absoluteEncoderID = 69;
-        
+
         public static final double CURRENT_SPIKE_THRESHOLD = 0;
         public static final double GEAR_REDUCTION = 75.0;
 
         public static final double MAX_ERROR = 3;
-
     }
-
-    
 
     public static final class IntakeConstants {
 
-            // CAN IDs and Ports
+        // CAN IDs and Ports
         public static final int INTAKE_PIVOT_PORT = 15;
         public static final int INTAKE_MOTOR_PORT = 19;
         public static final int INTAKE_PIVOT_2_PORT = 25;
         public static final int BEAM_BREAKER_PORT = 1;
 
-            // Motor Limits
+        // Motor Limits
         public static final int MAX_CURRENT = 45;
         public static final int SMART_MAX_CURRENT = 20;
-        public static final double CURRENT_SPIKE_THRESHOLD = 5.2; 
+        public static final double CURRENT_SPIKE_THRESHOLD = 5.2;
         public static final double INTAKE_MOTOR_SPEED = 0.5;
         public static final double INTAKE_PIVOT_SPEED = 0.3;
 
@@ -361,45 +363,52 @@ public final class Constants {
         public static final double INTAKE_DOWN_POSITION = 0;
         public static final double INTAKE_UP_POSITION = 0;
         */
-    
-            // Intake Pivot Constants
+
+        // Intake Pivot Constants
         public static final double kP_PIVOT = 0.01;
         public static final double kI_PIVOT = 0;
         public static final double kD_PIVOT = 0;
 
-		public static final double GEAR_REDUCTION = 82.8;
+        public static final double GEAR_REDUCTION = 82.8;
 
-            // Input Constants
+        // Input Constants
         private static final double kMaxVelocityDeg = 20;
         private static final double kMaxAccelDeg = 5;
         private static final double kMaxErrorDeg = 3;
 
-        private static final double DEPLOYED_POS_DEG =30;
+        private static final double DEPLOYED_POS_DEG = 30;
         private static final double RETRACTED_POS_DEG = 0;
-
 
         //  Do not edit these constants -------------
 
-            // Actual Constants
-        public static final double kMaxVelocity = pivotDegreesToMeters(kMaxVelocityDeg);
-        public static final double kMaxAccel = pivotDegreesToMeters(kMaxAccelDeg);
-        public static final double kMaxError = pivotDegreesToMeters(kMaxErrorDeg);
-        
-        public static final double DEPLOYED_POS = pivotDegreesToMeters(DEPLOYED_POS_DEG);
-        public static final double RETRACTED_POS = pivotDegreesToMeters(RETRACTED_POS_DEG);
+        // Actual Constants
+        public static final double kMaxVelocity = pivotDegreesToMeters(
+            kMaxVelocityDeg
+        );
+        public static final double kMaxAccel = pivotDegreesToMeters(
+            kMaxAccelDeg
+        );
+        public static final double kMaxError = pivotDegreesToMeters(
+            kMaxErrorDeg
+        );
+
+        public static final double DEPLOYED_POS = pivotDegreesToMeters(
+            DEPLOYED_POS_DEG
+        );
+        public static final double RETRACTED_POS = pivotDegreesToMeters(
+            RETRACTED_POS_DEG
+        );
 
         // -----------------------------------------
 
         /**
          * Converts degrees to the number of meters to feed as the targets for PID
-         * 
+         *
          * @param degrees   The target number of degrees
          * @return          The equivalent measurement in meters
          */
         private static double pivotDegreesToMeters(double degrees) {
             return (degrees / 360.0) / GEAR_REDUCTION;
         }
-        
     }
-
 }
