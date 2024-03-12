74b1d8a2f3301e520529def8d2f62f551fc3d7e4
Peyton S <mudzgamer5557@outlook.com>
2024-03-11 17:31:24 -0700

    paths and autos

--- src/main/java/frc/robot/util/AutoManager.java
+++ src/main/java/frc/robot/util/AutoManager.java
@@ -13,6 +13,7 @@ import com.pathplanner.lib.util.ReplanningConfig;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.SwerveSubsystem;
